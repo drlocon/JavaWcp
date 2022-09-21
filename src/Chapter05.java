77390737d24c7532b8b7643681ffa14ab4a404da
shiki <104292397+drlocon@users.noreply.github.com>
2022-09-21 12:16:48 +0900

    [Finish]5章確認問題

---
+++ src/Chapter05.java
@@ -0,0 +1,25 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 10000000000L;
+		System.out.println(l);
+		
+		double d = 1.23;
+		System.out.println(d);
+		
+		boolean b = true;
+		System.out.println(b);
+		
+		Integer i = 100;
+		System.out.println(i);
+		
+		Float f = 1.2345f;
+		System.out.println(f);
+		
+		Character c = 'あ';
+		System.out.println(c);
+		
+		int[] array = {10, 20, 30, 40, 50};
+		System.out.println(array[3]);
+	}
+}
