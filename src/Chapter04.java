9b521d716a848806ff03bd96d28b54df5dacfefa
shiki <104292397+drlocon@users.noreply.github.com>
2022-09-21 11:42:44 +0900

    [Finish]4章確認問題

---
+++ src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String name = "Nanashi San";
+		System.out.println(name);
+		name = "理性は羅針盤、情熱は疾風";
+		System.out.println(name);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
